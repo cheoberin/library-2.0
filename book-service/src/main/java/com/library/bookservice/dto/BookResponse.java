18080903f1c3a7f29f4a9c1dbabc0fe8e8eb8633
Airton <airton.manfrini@hotmail.com>
2023-03-16 21:57:04 -0300

    Fix: BookResponse

--- book-service/src/main/java/com/library/bookservice/dto/BookResponse.java
+++ book-service/src/main/java/com/library/bookservice/dto/BookResponse.java
@@ -2,10 +2,13 @@ package com.library.bookservice.dto;
 
 import com.library.bookservice.model.Book;
 
-public record BookResponse(String id, String name, int publicationYear, String asin) {
+import java.math.BigDecimal;
+import java.util.List;
+
+public record BookResponse(String id, String name, List<AuthorResponse> authors, String bookCover,
+                           BigDecimal price) {
 
     public BookResponse(Book book) {
-        this(book.get_id(), book.getName(), book.getPublicationYear(), book.getAsin());
+        this(book.get_id(), book.getName(), book.getAuthors().stream().map(AuthorResponse::new).toList(), book.getBookCover(), book.getPrice());
     }
-
 }
