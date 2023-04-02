8025c0c09085a7b52fa7b9fc7c9a5b33e29016b4
Airton <airton.manfrini@hotmail.com>
2023-04-02 19:58:38 -0300

    fix: removed unfinished end point that was preventing the application to start

--- order-service/src/main/java/com/library/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/library/orderservice/controller/OrderController.java
@@ -1,12 +1,7 @@
 package com.library.orderservice.controller;
 
-import com.library.orderservice.dto.OrderRequest;
 import com.library.orderservice.service.OrderService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +12,4 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping
-    public ResponseEntity create(@RequestBody @Valid OrderRequest orderRequest){
-
-
-    }
 }
