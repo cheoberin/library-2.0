a9b6cfcb48904ef1bc7e701adf5e48b01ec61057
Airton <airton.manfrini@hotmail.com>
2023-04-22 17:55:27 -0300

    add: small changes

--- auth-service/src/main/java/com/library/authservice/service/UserServiceImp.java
+++ auth-service/src/main/java/com/library/authservice/service/UserServiceImp.java
@@ -29,6 +29,9 @@ public class UserServiceImp implements UserService {
     public User saveUser(User user) {
         log.info("Saving new user {} to the database...", user.getName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+
+
         return userRepository.save(user);
     }
 
